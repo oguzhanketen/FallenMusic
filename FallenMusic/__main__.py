1f5cc65c9e6c7d0c9738782ee2333e6f6d9804a1
oguzhanketen <77228564+oguzhanketen@users.noreply.github.com>
2023-04-17 18:02:32 +0300

    Update __main__.py
    Signed-off-by: oguzhanketen <77228564+oguzhanketen@users.noreply.github.com>

--- FallenMusic/__main__.py
+++ FallenMusic/__main__.py
@@ -43,42 +43,42 @@ from FallenMusic.Modules import ALL_MODULES
 
 
 async def fallen_startup():
-    LOGGER.info("[•] Loading Modules...")
+    LOGGER.info("[•] Modüller Yükleniyor...")
     for module in ALL_MODULES:
         importlib.import_module("FallenMusic.Modules." + module)
     LOGGER.info(f"[•] Loaded {len(ALL_MODULES)} Modules.")
 
-    LOGGER.info("[•] Refreshing Directories...")
+    LOGGER.info("[•] Dizinler Yenileniyor...")
     if "downloads" not in os.listdir():
         os.mkdir("downloads")
     if "cache" not in os.listdir():
         os.mkdir("cache")
-    LOGGER.info("[•] Directories Refreshed.")
+    LOGGER.info("[•] Dizinler Yenilendi.")
 
     try:
         await app.send_message(
             SUNAME,
-            f"✯ ғᴀʟʟᴇɴ ᴍᴜsɪᴄ ʙᴏᴛ ✯\n\n𖢵 ɪᴅ : `{BOT_ID}`\n𖢵 ɴᴀᴍᴇ : {BOT_NAME}\n𖢵 ᴜsᴇʀɴᴀᴍᴇ : @{BOT_USERNAME}",
+            f"✯ ᴏɢᴢ ᴍᴜsɪᴄ ʙᴏᴛ ✯\n\n𖢵 ɪᴅ : `{BOT_ID}`\n𖢵 ᴀᴅɪ : {BOT_NAME}\n𖢵 ᴋᴜʟʟᴀɴɪᴄɪ ᴀᴅɪ : @{BOT_USERNAME}",
         )
     except:
         LOGGER.error(
-            f"{BOT_NAME} failed to send message at @{SUNAME}, please go & check."
+            f"{BOT_NAME} Şuraya mesaj gönderilmedi @{SUNAME}, lütfen kontrol edin."
         )
 
     try:
         await app2.send_message(
             SUNAME,
-            f"✯ ғᴀʟʟᴇɴ ᴍᴜsɪᴄ ᴀss ✯\n\n𖢵 ɪᴅ : `{ASS_ID}`\n𖢵 ɴᴀᴍᴇ : {ASS_NAME}\n𖢵 ᴜsᴇʀɴᴀᴍᴇ : @{ASS_USERNAME}",
+            f"✯ ᴏɢᴢ ᴍᴜsɪᴄ ᴀsɪsᴛᴀɴ ✯\n\n𖢵 ɪᴅ : `{ASS_ID}`\n𖢵 ᴀᴅɪ : {ASS_NAME}\n𖢵 ᴋᴜʟʟᴀɴɪᴄɪ ᴀᴅɪ : @{ASS_USERNAME}",
         )
     except:
         LOGGER.error(
-            f"{ASS_NAME} failed to send message at @{SUNAME}, please go & check."
+            f"{ASS_NAME} Şuraya mesaj gönderilmedi @{SUNAME}, lütfen kontrol edin."
         )
 
     await app2.send_message(BOT_USERNAME, "/start")
 
-    LOGGER.info(f"[•] Bot Started As {BOT_NAME}.")
-    LOGGER.info(f"[•] Assistant Started As {ASS_NAME}.")
+    LOGGER.info(f"[•] Bot başlatıldı {BOT_NAME}.")
+    LOGGER.info(f"[•] Asistan başlatıldı {ASS_NAME}.")
 
     LOGGER.info(
         "[•] \x53\x74\x61\x72\x74\x69\x6e\x67\x20\x50\x79\x54\x67\x43\x61\x6c\x6c\x73\x20\x43\x6c\x69\x65\x6e\x74\x2e\x2e\x2e"
@@ -89,4 +89,4 @@ async def fallen_startup():
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(fallen_startup())
-    LOGGER.error("Fallen Music Bot Stopped.")
+    LOGGER.error("OGZ Music Bot Durduruldu.")
