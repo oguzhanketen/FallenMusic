2319b237e32d6083e5ebb287dac8c67df4e63bf9
oguzhanketen <77228564+oguzhanketen@users.noreply.github.com>
2023-04-17 19:47:16 +0300

    Update activevc.py
    Signed-off-by: oguzhanketen <77228564+oguzhanketen@users.noreply.github.com>

--- FallenMusic/Modules/activevc.py
+++ FallenMusic/Modules/activevc.py
@@ -30,7 +30,7 @@ from FallenMusic.Helpers.inline import close_key
 
 @app.on_message(filters.command("activevc") & SUDOERS)
 async def activevc(_, message: Message):
-    mystic = await message.reply_text("» ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇᴄʜᴀᴛs ʟɪsᴛ...")
+    mystic = await message.reply_text("» ᴀᴋᴛɪғ sᴇsʟɪ sᴏʜʙᴇᴛ ʟɪsᴛᴇsɪ ᴀʟıɴıʏᴏʀ...")
     chats = await get_active_chats()
     text = ""
     j = 0
@@ -46,10 +46,10 @@ async def activevc(_, message: Message):
             text += f"<b>{j + 1}. {title}</b> [`{chat}`]\n"
         j += 1
     if not text:
-        await mystic.edit_text("ɴᴏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇᴄʜᴀᴛs ᴏɴ ᴍᴜsɪᴄʙᴏᴛ...")
+        await mystic.edit_text("ᴀᴋᴛɪғ sᴇsʟɪ sᴏʜʙᴇᴛ ʏᴏᴋ...")
     else:
         await mystic.edit_text(
-            f"**ʟɪsᴛ ᴏғ ᴄᴜʀʀᴇɴᴛʟʏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇᴄʜᴀᴛs ᴏɴ ᴍᴜsɪᴄ ʙᴏᴛ :**\n\n{text}",
+            f"**ᴀᴋᴛɪғ ᴏʟᴀɴ sᴇsʟɪ sᴏʜʙᴇᴛ ʟɪsᴛᴇsɪ :**\n\n{text}",
             reply_markup=close_key,
             disable_web_page_preview=True,
         )
