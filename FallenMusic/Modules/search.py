0fdd3fbdb3972129406402652193267aed4848ea
oguzhanketen <77228564+oguzhanketen@users.noreply.github.com>
2023-04-17 21:11:41 +0300

    Update search.py
    Signed-off-by: oguzhanketen <77228564+oguzhanketen@users.noreply.github.com>

--- FallenMusic/Modules/search.py
+++ FallenMusic/Modules/search.py
@@ -35,24 +35,24 @@ async def ytsearch(_, message: Message):
         pass
     try:
         if len(message.command) < 2:
-            return await message.reply_text("» ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ sᴇᴀʀᴄʜ ʙᴀʙʏ !")
+            return await message.reply_text("» ʙᴇʙᴇɢ̆ɪᴍ ᴀʀᴀᴍᴀᴋ ɪᴄ̧ɪɴ ʙɪşᴇʏʟᴇʀ ʏᴀᴢᴍᴀʟısıɴ !")
         query = message.text.split(None, 1)[1]
         m = await message.reply_text("🔎")
         results = YoutubeSearch(query, max_results=4).to_dict()
         i = 0
         text = ""
         while i < 4:
-            text += f"✨ ᴛɪᴛʟᴇ : {results[i]['title']}\n"
-            text += f"⏱ ᴅᴜʀᴀᴛɪᴏɴ : `{results[i]['duration']}`\n"
-            text += f"👀 ᴠɪᴇᴡs : `{results[i]['views']}`\n"
-            text += f"📣 ᴄʜᴀɴɴᴇʟ : {results[i]['channel']}\n"
+            text += f"✨ ʙᴀşʟıᴋ : {results[i]['title']}\n"
+            text += f"⏱ sᴜ̈ʀᴇ : `{results[i]['duration']}`\n"
+            text += f"👀 ɢᴏ̈ʀᴜ̈ɴᴛᴜ̈ʟᴇʏᴇɴ : `{results[i]['views']}`\n"
+            text += f"📣 ᴋᴀɴᴀʟ : {results[i]['channel']}\n"
             text += f"🔗 ʟɪɴᴋ : https://youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
         key = InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        text="ᴄʟᴏsᴇ",
+                        text="ᴋᴀᴘᴀᴛ",
                         callback_data=f"forceclose abc|{message.from_user.id}",
                     ),
                 ]
